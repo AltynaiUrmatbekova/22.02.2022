c2959022866c605aeb1120339f096776eb64cc97
AltynaiUrmatbekova <64131999+AltynaiUrmatbekova@users.noreply.github.com>
2022-02-22 11:36:22 +0600

    added type of color

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ document.querySelector('#text').addEventListener('input', function() {
 document.querySelector('#color').addEventListener('input', function() {
     h1.style.color = this.value;
 });
+document.querySelector('#background-color').addEventListener('input', function() {
+    h1.style.backgroundColor = this.value;
+});
+
+
